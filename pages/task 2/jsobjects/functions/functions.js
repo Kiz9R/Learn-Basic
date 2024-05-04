5e536b9c06a6d7f957fa6b344a18d010ffa8d6b6
Kiz9R <kiz9r>
2024-05-04 11:48:10 +0000

    task updated

--- pages/task 2/jsobjects/functions/functions.js
+++ pages/task 2/jsobjects/functions/functions.js
@@ -1,14 +1,38 @@
 export default {
 	
-	getFilteredStudent: () => {
-		
-		const result = getData.data;	
-		return result;
-	},
+	// getFilteredStudent: () => {
+		// 
+		// const result = getData.data;	
+		// return result;
+	// },
 	
-	manyUpdates:()=>{
-		const updates=Table1.updatedRows.map(update=>update.allFields);
-		return updates;
+	manyUpdates:async()=>{
+		const updates=Table1.updatedRows.map(i=>i.allFields);
+		
+		// return updates;
+		let res="";
+		
+		for (let update of updates) {
+		const { roll_number, student_name, score, time_taken_in_sec } = update;
+			
+			 res=`${roll_number}+" "+${student_name}+" "+${score}+" "+ ${time_taken_in_sec}`;
+			
+			
+
+		await updateStudentData.run({
+			roll_number: roll_number,
+			student_name: student_name
+		});
+
+		// await updateChapterData.run({
+			// roll_number: roll_number,
+			// score: score,
+			// time_taken_in_sec: time_taken_in_sec
+		// });
+	}
+		await getData.run();
+		return res;
+		
 	},
 
 	handleReset: async () => {
