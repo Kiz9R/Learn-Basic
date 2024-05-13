f08243d146bd158e4358875a896fc4268f2b93a4
Kiz9R <kiz9r>
2024-05-13 09:35:34 +0000

    Rest-Api error handling

--- pages/task 2/jsobjects/getData/getData.js
+++ pages/task 2/jsobjects/getData/getData.js
@@ -8,34 +8,37 @@ export default {
 			const chapterVal = chapter.selectedOptionValue;
 			const testVal = test.selectedOptionValue;
 
-			if (schoolVal && subjectVal && classVal && sectionVal && chapterVal && testVal) 
-				await getDataQl.run();
-			else {
-				return [];
+			let tableData=[];
+
+			if (schoolVal && subjectVal && classVal && sectionVal && chapterVal && testVal) {
+
+				await getStudentScoresRest.run();
+
+				const chapters = getStudentScoresRest.data.chapter;
+
+				tableData = chapters.map((chapter) => {
+					const student = chapter.Students;
+					return {
+						rollNumber: student.roll_number,
+						studentName: student.student_name,
+						score: chapter.score,
+						timeTaken: chapter.time_taken_in_sec
+					};
+				});
+
+				if (tableData.length===0) {
+					throw new Error('No data available, select fields properly');
+				}
 			}
 
-			const chapters = getDataQl.data.data.chapter;
-
-			if (!chapters || chapters.length === 0) {
-				throw new Error('No data available');
-			}
-
-			const tableData = chapters.map((chapter) => {
-				const student = chapter.Students;
-				return {
-					rollNumber: student.roll_number,
-					studentName: student.student_name,
-					score: chapter.score,
-					timeTaken: chapter.time_taken_in_sec
-				};
-			});
-
+			if(tableData.length>0)
+				showAlert("Data retrived","success")
 			return tableData;
 
 		} catch (err) {
-			return showAlert(`${err.message}`, 'error');
+			return showAlert(`${err.message} - 400 BAD_REQUEST`, 'error');
 		}
 	},
-
+// ${getStudentScoresRest.responseMeta.statusCode}
 
 }
--- pages/task 2/jsobjects/update/update.js
+++ pages/task 2/jsobjects/update/update.js
@@ -4,15 +4,15 @@ export default {
 
 		let studentUpdates = [];
 		let chapterUpdates = [];
-		
+
 		const chapterName=chapter.selectedOptionValue;
 		const subjectName=subject.selectedOptionValue;
 		const testName=test.selectedOptionValue;
 
 		for (let update of updates) {
 			const { rollNumber, studentName, score, timeTaken } = update;
-			
-			
+
+
 
 			const condition1 = { "roll_number": { "_eq": rollNumber } };
 			const condition2 = { "roll_number": { "_eq": rollNumber }, "_and": {subject: {"_eq":subjectName }, "_and": {"test_name": {"_eq": testName}, "_and": {chapter_name: {"_eq": chapterName}}}}} ;
@@ -25,16 +25,21 @@ export default {
 			studentUpdates.push(studentUpdate);
 			chapterUpdates.push(chapterUpdate); 
 		}
-		
+
 		try {
-			await updateStudentDataQl.run({ data: studentUpdates });
-			await updateChapterDataQl.run({ data: chapterUpdates });
+
+			await updateStudentDataRest.run({data: studentUpdates})
+			await updateChapterDataRest.run({data: chapterUpdates});
+
+			// await updateStudentDataRest.run({data: chapterUpdates})
+			// await updateChapterDataRest.run({data: studentUpdates})
 			
 			await getData.studentScore();
-			
-			return showAlert("Update successfull", "success");
+
+			return showAlert(`Update successfull - ${updateChapterDataRest.responseMeta.statusCode}`, "success");
 		} catch (error) {
-			return showAlert(`${error.message}`,"error");
+			await getData.studentScore();
+			return showAlert(`Data cannot be updated - 500 SERVER_ERROR`,"error");
 		}
 
 	}
